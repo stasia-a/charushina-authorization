513d1bae712baba478e1ea53499adfe5e4b06584
Anastasia Charushina <stasia-a@tut.by>
2012-10-04 17:51:21 +0300

    Add boolean column to User and changed some view for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :login, :password, :password_confirmation, :full_name
+  attr_accessible :email, :login, :password, :password_confirmation, :full_name, :admin
   has_secure_password
 
   validates :login, presence: true, uniqueness: true
@@ -10,12 +10,21 @@ class User < ActiveRecord::Base
   validates :full_name, length: { maximum: 26 }
 
   before_save { |user| user.email = email.downcase }
+  after_save {|user| first_user_is_admin}
   before_validation :assign_auth_secret, on: :create
 
   has_many :sessions
   accepts_nested_attributes_for :sessions
 
+  def first_user_is_admin(user)
+    user.admin = true if first_user?
+  end
+
   private
+
+  def first_user?
+    User.count == 1
+  end
 
   def assign_auth_secret
     self.auth_secret = ROTP::Base32.random_base32
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121004122630) do
+ActiveRecord::Schema.define(:version => 20121004143502) do
 
   create_table "sessions", :force => true do |t|
     t.integer  "user_id"
@@ -36,10 +36,11 @@ ActiveRecord::Schema.define(:version => 20121004122630) do
     t.string   "login"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "auth_secret"
     t.string   "full_name"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
